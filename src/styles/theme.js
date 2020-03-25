e17697264d6dc6163277d07ee0929f1028208b93
Romain <romain.rousseau5@gmail.com>
2020-03-25 13:32:09 +0100

    adding theme

---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+const Theme = {
+  secondary: '#FEBE81',
+  secondaryDark: '#e28420',
+  secondaryLight: '#FFD875',
+
+  primary: '#22CAAC',
+  primaryDark: '#001A20',
+
+  light: '#fafafa',
+  lightDark: '#ececec',
+
+  dark: '#181818',
+  darkLight: '#3a3a3a',
+
+  grey: '#bcbcbc',
+
+  fontFamily: 'Poppins',
+
+  cardRadius: '8px',
+};
+
+export default Theme;
