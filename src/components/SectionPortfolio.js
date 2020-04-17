dbbfd420be3f30487a7f3b0d0d4ec86410d94b8e
Romain <romain.rousseau5@gmail.com>
2020-04-17 13:41:45 +0200

    Modifying Portfolio Section Layout

---
+++ src/components/SectionPortfolio.js
@@ -0,0 +1,64 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import FlexboxColumn from './FlexboxColumn';
+import SectionTitle from './SectionTitle';
+import Separator from './Separator';
+import AnimatedComponentInView from './AnimatedComponentInView';
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
+  
+  @media (max-width: 1280px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+const Title = styled(FlexboxColumn)``;
+
+const CardGrid = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+class SectionPortfolio extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Grid>
+        <AnimatedComponentInView animationClass={'animated slideInUp'}>
+          <Title alignItems={'stretch'} justifyContent={'center'}>
+            <SectionTitle color={'#FFD875'}>Educational</SectionTitle>
+            <SectionTitle color={'#FEBE81'}>& Personal</SectionTitle>
+            <SectionTitle color={'#EFA45D'}>Projects</SectionTitle>
+            <Separator
+              color={'#22CAAC'}
+              margin={'2rem 0 1rem 0'}
+              width={'3rem'}
+            />
+          </Title>
+        </AnimatedComponentInView>
+        <CardGrid nbColumns={this.props.nbColumns}>
+          {this.props.children}
+        </CardGrid>
+      </Grid>
+    );
+  }
+}
+
+SectionPortfolio.propTypes = {
+  nbColumns: PropTypes.number,
+  title: PropTypes.string,
+};
+
+SectionPortfolio.defaultProps = {
+  title: 'Title',
+};
+
+export default SectionPortfolio;
