9e7b186bf784ce6fd3ce5e178b55bd99b29c8b93
Romain <romain.rousseau5@gmail.com>
2020-11-10 19:26:36 +0100

    updating navigation

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -1,6 +1,14 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import Breakpoints from "../../utils/breakpoints";
+import CaseStudyPreview from "../cards/CaseStudyPreview";
+import DropdownMenu from "../DropdownMenu";
+import FlexboxColumn from "../FlexboxColumn";
+import FlexboxRow from "../FlexboxRow";
+import Icon from "../Icon";
+import SocialIcons from "../SocialIcons";
+import Drawer from "./Drawer";
 import HeaderItem from "./HeaderItem";
 
 const Container = styled.header`
@@ -10,56 +18,179 @@ const Container = styled.header`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: center;
 
-  padding: 1rem;
+  padding: 1rem 0;
 
   width: 100%;
 
-  justify-content: ${props => (props.horizontal ? "flex-start" : "center")};
-  margin-left: ${props => (props.horizontal ? "2rem" : "")};
-
   z-index: 999;
 
-  background-color: ${props =>
-    props.horizontal ? "transparent" : props.theme.light};
-  border-bottom: ${props => (props.horizontal ? "" : "1px solid")};
-  border-color: ${props => (props.horizontal ? "" : props.theme.purpleLight)};
+  background-color: ${(props) => props.theme.light};
+
+  border-bottom: 1px solid ${(props) => props.theme.lightBlue};
 `;
 
 const Nav = styled.nav`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  max-width: 1280px;
+
+  padding: 0 2rem;
+
+  color: ${(props) => props.theme.black};
 `;
 
-function Header(props) {
+const NavLinks = styled(FlexboxRow)`
+  @media screen and (max-width: ${Breakpoints.md}px) {
+    display: none;
+  }
+`;
+
+const MenuLabel = styled.b`
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 24px;
+
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+`;
+
+const Sep = styled.div`
+  height: 100%;
+  width: 1px;
+
+  margin: 0 1rem;
+
+  background-color: ${(props) => props.theme.grey};
+`;
+
+const CaseStudyList = styled(FlexboxColumn)`
+  margin: 1rem 3rem 1rem 0;
+
+  p {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+const ProjectPreviews = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 1rem;
+
+  width: 100%;
+`;
+
+const Brand = styled(HeaderItem)`
+  && {
+    color: ${(props) => props.theme.primary};
+  }
+`;
+
+const CaseStudyLink = styled(HeaderItem)`
+  && {
+    font-weight: 500;
+
+    &:after {
+      display: none;
+    }
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const MenuIcon = styled(Icon)`
+  display: none;
+  cursor: pointer;
+
+  @media screen and (max-width: ${Breakpoints.md}px) {
+    display: flex;
+  }
+`;
+
+function Header({ caseStudies, featuredCases, className }) {
+  const [openMenu, setOpenMenu] = useState(false);
+  const [showDrawer, setShowDrawer] = useState(false);
+
+  const toggleDrawer = () => {
+    setShowDrawer(!showDrawer);
+  };
+
   return (
     <>
-      <Container horizontal={props.horizontal} className={props.className}>
+      <Container className={className}>
         <Nav>
-          {React.Children.toArray(props.children).map(section => (
+          <MenuIcon
+            name={showDrawer ? "close" : "menu"}
+            size={24}
+            color={"dark"}
+            onClick={() => toggleDrawer()}
+          />
+          <NavLinks alignItems={"center"}>
+            <Brand path={"/"} label={"rmnrss.io"} />
+            <HeaderItem path={"/"} label={"Home"} />
+            <HeaderItem path={"/#about-me"} label={"About Me"} />
             <HeaderItem
-              key={section.key}
-              horizontal={props.horizontal}
-              sectionID={section.props.id}
-              label={section.props.className}
+              path={"/#case-studies"}
+              label={"Case Studies"}
+              onMouseEnter={() => setOpenMenu(true)}
             />
-          ))}
+          </NavLinks>
+          <SocialIcons iconSize={24} />
         </Nav>
       </Container>
-      {props.children}
+
+      <Drawer
+        open={showDrawer}
+        toggleDrawer={toggleDrawer}
+        caseStudies={caseStudies}
+      />
+
+      <DropdownMenu onMouseLeave={() => setOpenMenu(false)} open={openMenu}>
+        <MenuLabel>Case Studies</MenuLabel>
+        <FlexboxRow alignItems={"flex-start"} justifyContent={"flex-start"}>
+          <CaseStudyList>
+            {caseStudies.map((el) => {
+              let obj = el.node;
+              return (
+                <CaseStudyLink
+                  key={obj.id}
+                  label={obj.title}
+                  path={`/case-studies/${obj.slug}`}
+                />
+              );
+            })}
+          </CaseStudyList>
+          <Sep />
+          <ProjectPreviews>
+            {featuredCases.map((aCase) => {
+              let obj = aCase.node;
+              return (
+                <CaseStudyPreview
+                  key={obj.id}
+                  title={obj.title}
+                  fluid={obj.cover.fluid}
+                  slug={obj.slug}
+                />
+              );
+            })}
+          </ProjectPreviews>
+        </FlexboxRow>
+      </DropdownMenu>
     </>
   );
 }
 
 Header.propTypes = {
-  children: PropTypes.node.isRequired,
   className: PropTypes.string,
-  horizontal: PropTypes.bool,
-};
-
-Header.defaultProps = {
-  horizontal: false,
+  caseStudies: PropTypes.arrayOf(PropTypes.object).isRequired,
+  featuredCases: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default Header;
