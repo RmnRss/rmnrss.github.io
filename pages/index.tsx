56c85320fcbc0bc46c5a515a418f45f961749385
rmnrss <romain.rousseau5@gmail.com>
2021-12-26 14:11:01 +0100

    added keyboard and cube to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Keyboard from "../components/Keyboard"
+import ProcrastinationCube from "../components/ProcrastinationCube"
 import SEO from "../components/seo"
 import FullscreenLayout from "../layouts/FullscreenLayout"
 
@@ -7,7 +9,22 @@ function IndexPage() {
       <SEO title="rmnrss.io" />
       <FullscreenLayout title="rmnrss.io">
         <div className="w-full h-full flex flex-col align-middle justify-start p-4">
-          <h1 className="text-2xl font-bold text-center">Hello there!</h1>
+          <h1 className="text-4xl font-bold text-lightBlue text-center mb-8">
+            Hello there!
+          </h1>
+          <b className="text-2xl font-medium text-center  mb-8">
+            Seems like this is still a work in progress...
+          </b>
+          <p className="text-lg text-lightBlue text-center">
+            While you wait feel free to play with the procrastination cub over
+            there
+          </p>
+          <div className="m-auto">
+            <ProcrastinationCube />
+          </div>
+          <div className="mt-auto align-bottom">
+            <Keyboard />
+          </div>
         </div>
       </FullscreenLayout>
     </body>
